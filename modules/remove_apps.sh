afddb1c81ef067941dc4f0c2d3ffc8da79f227ad
William Skellenger <wskellenger@gmail.com>
2017-01-08 12:45:09 -0500

    Default select blacklisted apps

--- modules/remove_apps.sh
+++ modules/remove_apps.sh
@@ -1,24 +1,30 @@
 # cd to dir so ls -d is working
 cd $WORKDIR/mount_path/app
 
+blacklist=$WORKDIR/modules/removeapplist.txt
 pkglist=""
 n=1
+
 for pkg in $(ls -d *)
 do
-        pkglist="$pkglist $pkg $n off"
-        n=$[n+1]
+   if grep -q $pkg "$blacklist"; then
+      pkglist="$pkglist $pkg $n on"
+   else
+      pkglist="$pkglist $pkg $n off"
+   fi
+
+   n=$[n+1]
 done
 
 cd $WORKDIR
 
 choices=`/usr/bin/dialog --stdout --checklist 'Choose APKs you want to REMOVE:' 80 80 80 $pkglist`
 
-
 if [ $? -eq 0 ]
 then
    for choice in $choices
    do
-          TEMP="$TEMP $choice"
+      TEMP="$TEMP $choice"
    done
 
    dialog --title "Remove unwanted APKs" \--yesno "Are you sure you want to remove \n $TEMP ?" 7 60
@@ -27,12 +33,12 @@ then
    case $response in
       0) echo "Deleting selected APKs"
 
-           for choice in $choices
-           do
-                   echo "Removing $choice" >> $LOGFILE 2>&1
-                   rm -rf $WORKDIR/mount_path/app/$choice >> $LOGFILE 2>&1
-           done
-      ;;
+         for choice in $choices
+         do
+            echo "Removing $choice" >> $LOGFILE 2>&1
+            rm -rf $WORKDIR/mount_path/app/$choice >> $LOGFILE 2>&1
+         done
+         ;;
    esac
 fi
 
